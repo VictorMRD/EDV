a660b8d580de393e295f71c79223cf7c4ae53a1a
VictorMRD <victormrd2002@hotmail.com>
2025-06-13 15:39:13 -0700

    Add shadcn to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div><p>this is my layout</p></div>
+        <div className="bg-white w-full">
+          <button></button>
+        </div>
         {children}
       </body>
     </html>
