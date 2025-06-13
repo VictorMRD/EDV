53357ae2713b8cafcd20136c8b4106164504e30f
VictorMRD <victormrd2002@hotmail.com>
2025-06-12 23:37:47 -0700

    Modify auth0 to add roles to session.user

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,17 @@
+import jwt from 'jsonwebtoken';
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+  async beforeSessionSaved(session, idToken) {
+    const decodedIdToken = jwt.decode(session.tokenSet.idToken)
+    const roles = decodedIdToken?.['https://edv.com/roles']
+
+    return {
+      ...session,
+      user: {
+        ...session.user,
+        user_role: (roles)
+      },
+    }
+  },
+})
