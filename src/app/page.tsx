1603fa1dd27b549f8d478ae4dde659d18ba4b662
VictorMRD <victormrd2002@hotmail.com>
2025-06-14 16:17:01 -0700

    Remove auth button from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
 import Link from 'next/link';
-import { auth0 } from "@/lib/auth0";
 import './globals.css';
 
 export default async function Home() {
-  const session = await auth0.getSession();
-
-  if (!session) {
-    return (
-      <main>
-        <div className="flex justify-center gap-4">
-          <Link href="/auth/login?screen_hint=signup">
-            <button>Registrate</button>
-          </Link>
-          <a href="/auth/login">
-            <button>Inicia sesión</button>
-          </a>
-        </div>
-      </main>
-    );
-  }
-
   return (
     <main>
-      <h1>Bienvenido, {session.user.name}!</h1>
-      <p>user role {session.user['https://edv.com/roles']}</p>
+      <h1>Bienvenido!</h1>
       <p>
-        <a href="/auth/logout">
-          <button>Cerrar sesión</button>
-        </a>
+        pagina principal lol
       </p>
     </main>
   );
