a69e84da292bcc2fb6fa5d304249df58a6063b57
VictorMRD <victormrd2002@hotmail.com>
2025-09-26 14:39:55 -0700

    Update index schema

--- src/app/db/schema/index.ts
+++ src/app/db/schema/index.ts
@@ -1,1 +1,4 @@
 export { publications } from "@/app/db/schema/publications.schema";
+export { users } from "@/app/db/schema/users.schema";
+export { roles } from "@/app/db/schema/roles.schema";
+export { rolesOfUsers } from "@/app/db/schema/rolesOfUsers.schema";
